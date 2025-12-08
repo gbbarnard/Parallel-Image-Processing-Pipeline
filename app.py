2f186bcfd32254bc2a20a7be8ee92a6c74523193
Hunter Parcells <hunterparcells@gmail.com>
2025-12-07 22:44:11 -0500

    chore: Add code comments.

--- app.py
+++ app.py
@@ -4,36 +4,39 @@ import time
 from PIL import Image, ImageFilter
 import multiprocessing as mp
 
-images_folder = "images"
-out_folder = "out"
+# Set constants.
+IMAGES_FOLDER = "images"
+OUTPUT_FOLDER = "out"
 
-if os.path.isdir(out_folder):
-  shutil.rmtree(out_folder)
-os.mkdir(out_folder)
+# Remove old output folder and its contents if it exists already.
+if os.path.isdir(OUTPUT_FOLDER):
+  shutil.rmtree(OUTPUT_FOLDER)
+os.mkdir(OUTPUT_FOLDER)
 
+# Apply filters to provided Pillow image.
 def apply_filters(image):
   out = image.filter(ImageFilter.BoxBlur(50))
   # out = out.filter(ImageFilter.BoxBlur(50))
 
   return out
 
+# Fully process a file in the images folder.
 def process_image(file):
-  with Image.open(f"{images_folder}/{file}") as image:
+  with Image.open(f"{IMAGES_FOLDER}/{file}") as image:
     out = apply_filters(image)
-    out.save(f"{out_folder}/{file}")
+    out.save(f"{OUTPUT_FOLDER}/{file}")
 
-os.path
-
-file_count = len(os.listdir(images_folder))
+file_count = len(os.listdir(IMAGES_FOLDER))
 print(f"\033[33mProcessing {file_count} file(s)...")
 
 start_time = time.time()
-files = os.listdir(images_folder)
+files = os.listdir(IMAGES_FOLDER)
+# The actual sequential execution.
 for i, file in enumerate(files):
   process_image(file)
   print(f"\033[33mProcessed {i+1}/{file_count} file(s)...")
 
+# Calculate runtime and rate.
 end_time = time.time()
 duration = end_time - start_time
-
 print(f"\033[32mDone! Processed {file_count} file(s) in {round(duration, 2)} seconds. Avg. {round((file_count / duration), 2)} files/s.\033[0m")
